88472468e602289c3ece82ce4f5e0bcb86965ddd
queen-x-vee <valentinabuoro4@gmail.com>
2022-06-24 14:27:18 +0100

    udacity project update

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
+const express = require('express')
 
 // Start up an instance of app
+const app = express()
+
+/*Dependencies*/
+const bodyParser = require('body-parser')
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors')
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
+//post function
+app.post('/add', async function (req, res){
+    const body = await req.body;
+    projectData = body
+
+    console.log('server side data', projectData)
+    res.status(200).send(projectData)
+
+    //date
+    //temperature
+    //feeling
+}
+ )
+
+ //get function
+ app.get('/all', async function(req, res){
+    res.send(projectData)
+ })
+
 // Setup Server
+const port = 8000
+
+const server = app.listen(port, ()=>{
+    console.log(`running on local host:${port}`)
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,137 @@
+
+
+
 /* Global Variables */
+const zip = document.querySelector('#zip')
+const generate = document.querySelector('#generate')
+const feelings = document.querySelector('#feelings')
+const thisDate = document.querySelector('#thisDate')
+const temp = document.querySelector('#temp')
+const content = document.querySelector('#content')
+const errors = document.querySelector('#errors')
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const example = 'https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}'
+const baseURI = 'https://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey  = '&appid=3a6f932d936dc40b2f40cc03e4125cfb'
+
+generate.addEventListener('click',(event)=>{
+    event.preventDefault()
+    const madeURI = `${baseURI}${zip.value}${apiKey}`
+    console.log(madeURI)
+    getData(madeURI) //getData is an asynchronous promise, so it can be cained with a .then and use the data that was returned
+    .then((data)=>{
+        cureData(data) //cureData is to remove specific information
+        //this second .then is used after we've gotten the required data
+        .then((info)=>{
+            postData("/add", info)
+            .then((data)=>{
+                retrieveData("/all")
+                .then((data)=>{
+                    updatedUI(data)
+                })
+            })
+        })
+    })
+})
+
+//the url is a placeholder
+const getData = async(url)=>{
+    try{
+        const result = await fetch(url);
+        //result will await and have a value that is a json file
+        const data = await result.json()
+        if(data.cod == 200)//cod is a key in the json file
+        {
+            //console.log(data)
+            return data
+
+        }else{
+            console.log(data.message) //message is a key in the json file
+        }
+        
+    }catch(e){
+        //if any error happens
+        console.log(e)
+    }
+}
+
+
+const cureData = async (data)=>{
+    try{
+        if(data.message){
+         const info = data.message
+            //console.log(data)
+            return info
+        }else{
+            //info is what we require to send to the data
+            const info  = {
+                date: newDate,
+                feelings: feelings.value,
+                temp: data.main.temp
+            }
+            //console.log(info)
+            return info
+        }
+    }catch(er){
+        console.error(er)
+    }
+    
+}
+
+const postData = async(url="", data={})=>{
+    try{
+        const result = await fetch(url, {
+            method: "POST",
+            credentials: "same-origin",
+            headers:{
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+        return result
+    }catch(er){console.error(er)}
+}
+
+const retrieveData = async (url)=>{
+    const data = await fetch(url)
+    try{
+        const respond = await data.json()
+        console.log(respond)
+        return respond
+    }catch(err){
+        console.error(err)
+    }
+}
+
+const updatedUI = async (data)=>{
+    const request = await data //we do not use data.json because the data is already an object so we dont need to make it .json
+    if(request.date){ //if date then make this changes
+        document.querySelector('#entryHolder').style.cssText = `
+        display:block;
+        font-size: 2rem;
+        border: 1px solid white;
+        padding: 1rem;`
+        thisDate.innerHTML = "Date: " + request.date
+        temp.innerHTML =  "Temperature: " + Math.round(request.temp)+ 'degrees';
+        content.innerHTML = request.feelings? "Content: "+ request.feelings: "what's your mood today?"
+        //document.querySelector('#entryHolder').style.display = "none"
+        document.querySelector('#errors').style.display = "none"
+        console.log(request)
+
+
+    }else{
+        document.querySelector('#entryHolder').style.display = "none"
+        document.querySelector('#errors').style.cssText = `
+        display:block;
+        font-size: 2rem;
+        border: 1px solid white;
+        padding: 1rem;
+        `
+        document.querySelector('#errors').innerHTML = 'incorrect entry'
+        
+    }
+    
+}
